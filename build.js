9afd6dbb1ffb46899c5939f6858edceece624a91
Bobby Brennan (EC2) <bobby.brennan@gmail.com>
2014-10-21 04:09:19 +0000

    Get tarballs working again

--- build.js
+++ build.js
@@ -8,59 +8,122 @@ var AUTH = require('./auth.js');
 var SERVER = require('./server.js');
 
 var ignoreFile = function(file) {
-  return file.match('/$') || file.match('.*README.md');
+  return file.match(/\.tgz$/) || file.match('/$') || file.match('.*README.md');
 }
 
-var FILES_TO_RENDER = [];
-var FILES_RENDERED = [];
+var FILES_TO_PROCESS = [];
+var FILES_PROCESSED = [];
+var DEST_DIR = '.lucytmp';
 
-var renderAllFiles = function(config, dirPrefix, onDone) {
-  for (var i = 0; i < FILES_TO_RENDER.length; ++i) {
-    var oldFilename = FILES_TO_RENDER[i];
-    var newFilename = oldFilename.substring(dirPrefix.length);
-    FS.readFile(oldFilename, 'utf8', function(err, data) {
+var renderAllFiles = function(config, onDone) {
+  console.log('render and kill');
+  FILES_TO_PROCESS = [];
+  FILES_PROCESSED = [];
+  GLOB(DEST_DIR + '/**', {mark: true}, function(err, files) {
+      for (var i = 0; i < files.length; ++i) {
+        if (!ignoreFile(files[i])) {
+          var from  = files[i];
+          var to = from.substring(dirPrefix.length);
+          FILES_TO_PROCESS.push({from: from, to: to, method: 'render'});
+        }
+      }
+      processFilesInQueue(config, onDone); 
+  });
+}
+
+var renderAndCopyFiles = function(config, onDone) {
+  FS.readFile(DEST_DIR + '/package.json', function(err, data) {
+    var packageDef = {};
+    try {
+      packageDef = JSON.parse(data);
+    } catch (e) {
+      console.log('Error parsing package.json');
+      throw e;
+    }
+    if (!packageDef.files) {
+      console.log('No files to copy/render');
+      return onDone();
+    }
+    alterSourcePaths(packageDef.files);
+    FILES_TO_PROCESS = packageDef.files;
+    FILES_PROCESSED = [];
+    processFilesInQueue(config, onDone);
+  });
+}
+
+var renderFile = function(map, config, onDone) {
+    console.log('rendering:' + JSON.stringify(map));
+    FS.readFile(map.from, {encoding: 'utf8'}, function(err, data) {
       if (err) {
-        console.log('error reading file:' + oldFilename);
+        console.log('error reading file:' + map.from);
         throw err;
       }
+      console.log('rendering:' + data);
       var rendered = "";
       try {
         rendered = EJS.render(data, config);
       } catch (e) {
         throw "Failed to render!" + e;
       }
-      FS.writeFile(newFilename, rendered, 'utf8', function() {
-        FILES_RENDERED.push(newFilename);
-        if (FILES_TO_RENDER.length === FILES_RENDERED.length) {
-          onDone();
-        }
+      FS.writeFile(map.to, rendered, function(err) {
+        if (err) {throw err}
+        console.log('wrote file:' + map.to);
+        onDone(map.to);
       });
     });
+}
+
+var copyFile = function(map, onDone) {
+  console.log('copying:' + JSON.stringify(map));
+  FS.readFile(map.from, function(err, data) {
+    if (err) {throw err}
+    FS.writeFile(map.to, data, function(err) {
+      if (err) {throw err}
+      onDone(map.to);
+    });
+  });
+}
+
+var processFilesInQueue = function(config, onDone) {
+  for (var i = 0; i < FILES_TO_PROCESS.length; ++i) {
+    var onDone = function(newFile) {
+      FILES_PROCESSED.push(newFile);
+      if (FILES_PROCESSED.length === FILES_TO_PROCESS.length) {
+        onDone();
+      }
+    };
+    if (FILES_TO_PROCESS[i].method === 'render') {
+      renderFile(FILES_TO_PROCESS[i], config, onDone);
+    } else {
+      copyFile(FILES_TO_PROCESS[i], onDone);
+    }
   }
 }
 
-var DEST_DIR = '.lucytmp';
+var alterSourcePaths = function(maps) {
+  for (var i = 0; i < maps.length; ++i) {
+    maps[i].from = DEST_DIR + '/' + maps[i].from;
+  }
+}
+
 var TAR_FILENAME = DEST_DIR + '/package.tgz';
 var runForPackage = function(packageName, config) {
   AUTH.login(function(email, password) {
     var maybeHandleErr = function(err) {
-      //recursiveRmdir(DEST_DIR);
-      if (err) throw err;
+      if (err) {
+        recursiveRmdir(DEST_DIR);
+        throw err;
+      }
     }
     FS.mkdir(DEST_DIR, function (err) {
       maybeHandleErr(err);
-      SERVER.getPackage(email, password, packageName, function(err, data) {
+      var writeStream = FS.createWriteStream(TAR_FILENAME, {encoding: 'binary'});
+      SERVER.getPackage(email, password, packageName, writeStream, function(err, data) {
         maybeHandleErr(err);
-        console.log('got package!');
-        FS.writeFile(TAR_FILENAME, data, {encoding: 'binary'}, function (err) {
-           maybeHandleErr(err);
-          console.log('wrote tar');
-          EXEC('tar xzf ' + TAR_FILENAME, function(err, stdout, stderr) {
-            maybeHandleErr(err);
-            console.log('untarred');
-            renderAllFiles(config, DEST_DIR + '/', function() {
-              recursiveRmdir(DEST_DIR);
-            });
+        EXEC('tar xzf ' + TAR_FILENAME + ' -C ' + DEST_DIR + '/', function(err, stdout, stderr) {
+          maybeHandleErr(err);
+          renderAndCopyFiles(config, function() {
+            recursiveRmdir(DEST_DIR);
           });
         });
       });
@@ -70,20 +133,14 @@ var runForPackage = function(packageName, config) {
 
 var runForRepo = function(repoLoc, config) {
   Repository.clone(repoLoc, DEST_DIR, function(err, repo) {
-    GLOB(DEST_DIR + '/**', {mark: true}, function(err, files) {
-      for (var i = 0; i < files.length; ++i) {
-        if (!ignoreFile(files[i])) {
-          FILES_TO_RENDER.push(files[i]);
-        }
-      }
-      renderAllFiles(config, DEST_DIR + '/', function() {
-        recursiveRmdir(DEST_DIR);
-      });
+    renderAndCopyFiles(config, function() {
+      recursiveRmdir(DEST_DIR);
     });
   });
 }
 
 var recursiveRmdir = function(dirName) {
+  console.log('rmdir:' + dirName);
   var list = FS.readdirSync(dirName);
   for (var i = 0; i < list.length; i++) {
     var filename = PATH.join(dirName, list[i]);
@@ -107,7 +164,7 @@ exports.run = function(args) {
   if (!source || !config) {
     return true;
   }
-  FS.readFile(config, 'utf8', function(err, data) {
+  FS.readFile(config, function(err, data) {
     if (err) {
       console.log('error reading from config:' + config);
       throw err;
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 var REQUEST = require('request');
-var FS = require('fs');
 
 var LUCY_HOST = 'http://54.213.18.26';
 var LUCY_PORT = 3000;
 var LUCY_URL = LUCY_HOST + ':' + LUCY_PORT;
 
-var handleResponse = function(callback) {
+var handleResponse = function(callback, ignoreBody) {
   return function(err, res, body) {
     if (err) { 
       console.log('Error connecting to Lucy\'s servers:' + JSON.stringify(err) + '::' + JSON.stringify(res));
-    } else {
+    } else if (!ignoreBody) {
       console.log(body);
     }
     if (callback) {
-      callback(res, body);
+      callback(err, body);
     }
   }
 }
 
 exports.publish = function(email, password, pkgDef, tarball) {
-  FS.writeFile('publishtmp.tgz', tarball, {encoding: 'binary'});
   REQUEST({
        method: 'POST',
        preambleCRLF: true,
@@ -46,18 +44,18 @@ exports.addUser = function(loginInfo) {
 
 exports.define = function(email, password, defn) {
   REQUEST({
-        url: GLOBAL.LUCY_URL + '/define',
+        url: LUCY_URL + '/define',
         json: true,
-        body: {email: email, password: password, payload: data},
+        body: {email: email, password: password, payload: defn},
         method: 'POST',
  }, handleResponse());
 }
 
-exports.getPackage = function(email, password, packageName, onData) {
+exports.getPackage = function(email, password, packageName, writeStream, onDone) {
   REQUEST({
     url: LUCY_URL + '/getPackage',
     json: true,
     body: {email: email, password: password, payload: packageName},
     method: 'POST',
-  }, handleResponse(onData));
+  }, handleResponse(onDone, true)).pipe(writeStream);
 }
